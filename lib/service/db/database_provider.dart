ad86e0854bfc797b0bd785c4f7e012991e12d98d
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-07 11:52:49 +0430

    +singleton db

---
+++ lib/service/db/database_provider.dart
@@ -0,0 +1,28 @@
+
+import 'package:omega/service/db/dao/notification_dao.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseProvider {
+  static final _instance = DatabaseProvider._internal();
+  static DatabaseProvider get = _instance;
+  bool isInit = false;
+  Database _db;
+
+  DatabaseProvider._internal();
+
+  Future<Database> db() async {
+    if (!isInit) await _init();
+    return _db;
+  }
+
+  Future _init() async {
+    var dbPath = await getDatabasesPath();
+    String path = join(dbPath, 'omega.db');
+
+    _db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+          await db.execute(NotificationDao().createTableQuery);
+        });
+  }
+}
