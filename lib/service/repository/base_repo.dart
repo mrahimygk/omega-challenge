b501befa94f94b16adacb481a481064ad9b299b8
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-07 17:30:40 +0430

    +base repo

---
+++ lib/service/repository/base_repo.dart
@@ -0,0 +1,15 @@
+import 'package:omega/service/db/database_provider.dart';
+
+abstract class BaseRepo<T> {
+  DatabaseProvider databaseProvider;
+
+  Future insert(T data);
+
+  Future update(T data);
+
+  Future delete(T data);
+
+  Future<T> getFromDb();
+
+  Future<List<T>> getAllFromDb();
+}
